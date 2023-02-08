310c48c5500d0f69e78fd9f9dbb640cfdc54b5d3
Sean Cowans <zedesia@yahoo.com>
2023-02-08 10:52:57 -0700

    Fix dealership inventory and inventory count

--- lib/dealership.rb
+++ lib/dealership.rb
@@ -1,5 +1,14 @@
-class Dealership
-  def initialize(name, address)
+require_relative 'car'
 
+class Dealership
+  attr_reader :cars, :inventory, :inventory_count
+  def initialize(name, address)
+    @inventory = []
+    @inventory_count = 0
+  end
+
+  def add_car(cars)
+    @inventory << cars
+    @inventory_count += 1
   end
 end
